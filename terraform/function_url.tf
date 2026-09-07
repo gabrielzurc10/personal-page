0887b80086c8bec17ebc44e65bfbd82a8af76a3b
Gabriel Cruz <Gabriel102697_cruz@yahoo.com>
2026-09-07 16:33:48 -0500

    update terraform

--- terraform/function_url.tf
+++ terraform/function_url.tf
@@ -8,11 +8,23 @@ resource "aws_lambda_function_url" "backend" {
   invoke_mode        = "RESPONSE_STREAM"
 }
 
-# Public (unauthenticated) access to the Function URL.
+# Public (unauthenticated) access to the Function URL. Since October 2025 a
+# NONE-auth URL needs BOTH grants below, otherwise every request gets a 403:
+# https://docs.aws.amazon.com/lambda/latest/dg/urls-auth.html#urls-auth-none
 resource "aws_lambda_permission" "function_url" {
   statement_id           = "AllowPublicFunctionUrlInvoke"
   action                 = "lambda:InvokeFunctionUrl"
   function_name          = aws_lambda_function.backend.function_name
   principal              = "*"
   function_url_auth_type = "NONE"
+}
+
+# The InvokedViaFunctionUrl condition keeps this grant limited to URL calls, so
+# it does not let arbitrary AWS principals invoke the function directly.
+resource "aws_lambda_permission" "function_url_invoke" {
+  statement_id             = "AllowPublicFunctionUrlInvokeFunction"
+  action                   = "lambda:InvokeFunction"
+  function_name            = aws_lambda_function.backend.function_name
+  principal                = "*"
+  invoked_via_function_url = true
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      # >= 6.x for aws_lambda_permission.invoked_via_function_url (see function_url.tf).
+      version = "~> 6.0"
     }
   }
 
